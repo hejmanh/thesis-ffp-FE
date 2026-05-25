c124074d7bee866bc45fff44d695117bffddfe7e
hejmanh <10422009@student.vgu.edu.vn>
2026-05-25 16:39:14 +0700

    config query client

--- lib/queryClient.ts
+++ lib/queryClient.ts
@@ -5,6 +5,16 @@ export const queryClient = new QueryClient({
     queries: {
       retry: 1,
       refetchOnWindowFocus: false,
+
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      gcTime: 10 * 60 * 1000, // 10 minutes
+
+      refetchOnReconnect: true,
+      throwOnError: true, // bubble errors to the nearest error boundary instead of returning them in the query result
+    },
+    mutations: {
+      retry: 0, // not retry post, put, delete
+      throwOnError: true,
     },
   },
 });
