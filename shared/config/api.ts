b9f352b2a4736d0871a2d80d3294d08929bc0634
dpkl <dpkl@netcompany.com>
2026-06-13 22:34:04 +0700

    Fix base url api

--- shared/config/api.ts
+++ shared/config/api.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
   baseURL:
-    process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:3000/api/v1",
+    process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:5000/api/v1",
 };
 
 export const TIMEOUT = 15_000;
