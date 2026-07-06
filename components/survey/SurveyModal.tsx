574be46e7a9c355b7a2977a790b3c5253ab6f232
Linh Dang Phan Khanh <khanhlinhdang2301@gmail.com>
2026-07-07 02:26:28 +0700

    Fix scroll behaviour

--- components/survey/SurveyModal.tsx
+++ components/survey/SurveyModal.tsx
@@ -63,8 +63,6 @@ export default function SurveyModal({ isOpen, onClose }: SurveyModalProps) {
     }
 
     const originalOverflow = document.body.style.overflow;
-    document.body.style.overflow = "hidden";
-    window.addEventListener("keydown", handleFocusTrap);
 
     return () => {
       window.removeEventListener("keydown", handleFocusTrap);
